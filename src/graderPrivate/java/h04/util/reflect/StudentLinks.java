bece2bb4eb8dd08444b674985eaab89223738046
Daniel Mangold <mangold.daniel@protonmail.com>
2024-01-05 12:58:25 +0100

    Fix static initializer errors

--- src/graderPrivate/java/h04/util/reflect/StudentLinks.java
+++ src/graderPrivate/java/h04/util/reflect/StudentLinks.java
@@ -9,7 +9,6 @@ import fopbot.Robot;
 import org.tudalgo.algoutils.tutor.general.assertions.Assertions2;
 import org.tudalgo.algoutils.tutor.general.assertions.Assertions3;
 import org.tudalgo.algoutils.tutor.general.callable.ObjectCallable;
-import org.tudalgo.algoutils.tutor.general.match.BasicReflectionMatchers;
 import org.tudalgo.algoutils.tutor.general.match.Matcher;
 import org.tudalgo.algoutils.tutor.general.reflections.*;
 
@@ -32,44 +31,44 @@ public class StudentLinks {
         ));
     }
 
-    private static Supplier<BasicMethodLink> methodLinkSup(TypeLink tl, String name) {
+    private static Supplier<BasicMethodLink> methodLinkSup(Supplier<? extends TypeLink> tl, String name) {
         return Suppliers.memoize(() -> (BasicMethodLink) Assertions3.assertMethodExists(
-            tl,
+            tl.get(),
             similarityMatcher(name)
         ));
     }
 
-    private static Supplier<BasicFieldLink> fieldLinkSup(TypeLink tl, Matcher<FieldLink> matcher) {
+    private static Supplier<BasicFieldLink> fieldLinkSup(Supplier<? extends TypeLink> tl, Matcher<FieldLink> matcher) {
         return Suppliers.memoize(() -> (BasicFieldLink) Assertions3.assertFieldExists(
-            tl,
+            tl.get(),
             matcher
         ));
     }
 
-    private static Supplier<BasicFieldLink> fieldLinkSup(TypeLink tl, String name) {
+    private static Supplier<BasicFieldLink> fieldLinkSup(Supplier<? extends TypeLink> tl, String name) {
         return fieldLinkSup(tl, similarityMatcher(name));
     }
 
-    public static Supplier<BasicConstructorLink> constructorLinkSup(TypeLink tl, Matcher<ConstructorLink> matcher) {
+    public static Supplier<BasicConstructorLink> constructorLinkSup(Supplier<? extends TypeLink> tl, Matcher<ConstructorLink> matcher) {
         return Suppliers.memoize(() -> (BasicConstructorLink) Assertions3.assertConstructorExists(
-            tl,
+            tl.get(),
             matcher
         ));
     }
 
-    public static Supplier<BasicConstructorLink> constructorLinkSup(TypeLink tl, TypeLink... args) {
-        return constructorLinkSup(tl, BasicReflectionMatchers.sameTypes(args));
+    public static Supplier<BasicConstructorLink> constructorLinkSup(Supplier<? extends TypeLink> tl, Supplier<? extends TypeLink>... args) {
+        return constructorLinkSup(tl, sameTypes(args));
     }
 
-    public static Supplier<BasicConstructorLink> constructorLinkSup(TypeLink tl, ObjectCallable<Class<?>>... args) {
+    public static Supplier<BasicConstructorLink> constructorLinkSup(Supplier<? extends TypeLink> tl, ObjectCallable<Class<?>>... args) {
         return constructorLinkSup(tl, Arrays.stream(args)
             .map(l -> Assertions2.callObject(
                 l,
                 Assertions2.emptyContext(),
                 r -> "The desired Class could not be retrieved. This is likely an Error in the tests."
             ))
-            .map(BasicTypeLink::of)
-            .toArray(TypeLink[]::new));
+            .map(clazz -> (Supplier<? extends TypeLink>) () -> BasicTypeLink.of(clazz))
+            .toArray(Supplier[]::new));
     }
 
     private static <T extends FieldLink> Matcher<T> arrayLikeMatcher(Class<?> clazz) {
@@ -91,105 +90,123 @@ public class StudentLinks {
         );
     }
 
+    public static <T extends WithType> Matcher<T> sameType(Supplier<? extends TypeLink> link) {
+        try {
+            return Matcher.of((l) -> Objects.equals(l.type(), link.get()), String.format("Same type: %s", link.get().reflection().getName()));
+        } catch (Throwable t) {
+            return Matcher.never();
+        }
+    }
+
+    public static <T extends WithTypeList> Matcher<T> sameTypes(Supplier<? extends TypeLink>... types) {
+        try {
+            List<? extends TypeLink> parameterList = Arrays.stream(types).map(Supplier::get).toList();
+            return Matcher.of((l) -> l.typeList().equals(parameterList),
+                String.format("Same parameter types: %s", parameterList.stream().map(TypeLink::name).toList()));
+        } catch (Throwable t) {
+            return Matcher.never();
+        }
+    }
+
     // MoveStrategy
     public static final Supplier<BasicTypeLink> MOVE_STRATEGY_LINK = typeLinkSup("MoveStrategy");
     public static final Supplier<BasicMethodLink> MOVE_STRATEGY_START_LINK = methodLinkSup(
-        MOVE_STRATEGY_LINK.get(),
+        MOVE_STRATEGY_LINK,
         "start"
     );
 
     // MoveStrategyWithCounter
     public static final Supplier<BasicTypeLink> MOVE_STRATEGY_WITH_COUNTER_LINK = typeLinkSup("MoveStrategyWithCounter");
     public static final Supplier<BasicMethodLink> MOVE_STRATEGY_WITH_COUNTER_GET_MOVE_COUNT_LINK = methodLinkSup(
-        MOVE_STRATEGY_WITH_COUNTER_LINK.get(),
+        MOVE_STRATEGY_WITH_COUNTER_LINK,
         "getMoveCount"
     );
 
     // MoveByTeleport
     public static final Supplier<BasicTypeLink> MOVE_BY_TELEPORT_LINK = typeLinkSup("MoveByTeleport");
     public static final Supplier<BasicMethodLink> MOVE_BY_TELEPORT_START_LINK = methodLinkSup(
-        MOVE_BY_TELEPORT_LINK.get(),
+        MOVE_BY_TELEPORT_LINK,
         "start"
     );
 
     // MoveByWalk
     public static final Supplier<BasicTypeLink> MOVE_BY_WALK_LINK = typeLinkSup("MoveByWalk");
     public static final Supplier<BasicMethodLink> MOVE_BY_WALK_START_LINK = methodLinkSup(
-        MOVE_BY_WALK_LINK.get(),
+        MOVE_BY_WALK_LINK,
         "start"
     );
     public static final Supplier<BasicMethodLink> MOVE_BY_WALK_GET_MOVE_COUNT_LINK = methodLinkSup(
-        MOVE_BY_WALK_LINK.get(),
+        MOVE_BY_WALK_LINK,
         "getMoveCount"
     );
 
     // FieldSelectorListener
     public static final Supplier<BasicTypeLink> FIELD_SELECTOR_LISTENER_LINK = typeLinkSup("FieldSelectionListener");
     public static final Supplier<BasicMethodLink> FIELD_SELECTOR_LISTENER_ON_FIELD_SELECTION_LINK = methodLinkSup(
-        FIELD_SELECTOR_LISTENER_LINK.get(),
+        FIELD_SELECTOR_LISTENER_LINK,
         "onFieldSelection"
     );
 
     // RobotMover
     public static final Supplier<BasicTypeLink> ROBOT_MOVER_LINK = typeLinkSup("RobotMover");
     public static final Supplier<BasicFieldLink> ROBOT_MOVER_ROBOTS_LINK = fieldLinkSup(
-        ROBOT_MOVER_LINK.get(),
+        ROBOT_MOVER_LINK,
         arrayLikeMatcher(Robot.class)
     );
     public static final Supplier<BasicFieldLink> ROBOT_MOVER_MOVE_STRATEGY_LINK = fieldLinkSup(
-        ROBOT_MOVER_LINK.get(),
-        BasicReflectionMatchers.sameType(MOVE_STRATEGY_LINK.get())
+        ROBOT_MOVER_LINK,
+        sameType(MOVE_STRATEGY_LINK)
     );
     public static final Supplier<BasicMethodLink> ROBOT_MOVER_ADD_ROBOT_LINK = methodLinkSup(
-        ROBOT_MOVER_LINK.get(),
+        ROBOT_MOVER_LINK,
         "addRobot"
     );
     public static final Supplier<BasicMethodLink> ROBOT_MOVER_ON_FIELD_SELECTION_LINK = methodLinkSup(
-        ROBOT_MOVER_LINK.get(),
+        ROBOT_MOVER_LINK,
         "onFieldSelection"
     );
     public static final Supplier<BasicConstructorLink> ROBOT_MOVER_CONSTRUCTOR_LINK = constructorLinkSup(
-        ROBOT_MOVER_LINK.get(),
-        MOVE_STRATEGY_LINK.get()
+        ROBOT_MOVER_LINK,
+        MOVE_STRATEGY_LINK
     );
 
     // MoveableRobot
     public static final Supplier<BasicTypeLink> MOVEABLE_ROBOT_LINK = typeLinkSup("MoveableRobot");
     public static final Supplier<BasicFieldLink> MOVEABLE_ROBOT_MOVE_STRATEGY_LINK = fieldLinkSup(
-        MOVEABLE_ROBOT_LINK.get(),
-        BasicReflectionMatchers.sameType(MOVE_STRATEGY_LINK.get())
+        MOVEABLE_ROBOT_LINK,
+        sameType(MOVE_STRATEGY_LINK)
     );
     public static final Supplier<BasicMethodLink> MOVEABLE_ROBOT_ADD_ROBOT_LINK = methodLinkSup(
-        MOVEABLE_ROBOT_LINK.get(),
+        MOVEABLE_ROBOT_LINK,
         "addRobot"
     );
     public static final Supplier<BasicMethodLink> MOVEABLE_ROBOT_ON_FIELD_SELECTION_LINK = methodLinkSup(
-        MOVEABLE_ROBOT_LINK.get(),
+        MOVEABLE_ROBOT_LINK,
         "onFieldSelection"
     );
     public static final Supplier<BasicConstructorLink> MOVEABLE_ROBOT_CONSTRUCTOR_LINK = constructorLinkSup(
-        MOVEABLE_ROBOT_LINK.get(),
-        MOVE_STRATEGY_LINK.get()
+        MOVEABLE_ROBOT_LINK,
+        MOVE_STRATEGY_LINK
     );
 
     // FieldSelector
     public static final Supplier<BasicTypeLink> FIELD_SELECTOR_LINK = typeLinkSup("FieldSelector");
     public static final Supplier<BasicMethodLink> FIELD_SELECTOR_SET_FIELD_SELECTION_LISTENER_LINK = methodLinkSup(
-        FIELD_SELECTOR_LINK.get(),
+        FIELD_SELECTOR_LINK,
         "setFieldSelectionListener"
     );
 
     // MouseFieldSelector
     public static final Supplier<BasicTypeLink> MOUSE_FIELD_SELECTOR_LINK = typeLinkSup("MouseFieldSelector");
     public static final Supplier<BasicMethodLink> MOUSE_FIELD_SELECTOR_SET_FIELD_SELECTION_LISTENER_LINK = methodLinkSup(
-        MOUSE_FIELD_SELECTOR_LINK.get(),
+        MOUSE_FIELD_SELECTOR_LINK,
         "setFieldSelectionListener"
     );
 
     // KeyboardFieldSelector
     public static final Supplier<BasicTypeLink> KEYBOARD_FIELD_SELECTOR_LINK = typeLinkSup("KeyboardFieldSelector");
     public static final Supplier<BasicMethodLink> KEYBOARD_FIELD_SELECTOR_SET_FIELD_SELECTION_LISTENER_LINK = methodLinkSup(
-        KEYBOARD_FIELD_SELECTOR_LINK.get(),
+        KEYBOARD_FIELD_SELECTOR_LINK,
         "setFieldSelectionListener"
     );
 }
